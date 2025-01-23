39e3b06a992554b9e7871d1e62f6d607e337e8b2
chrisezedi <ekenechrisezedi@gmail.com>
2025-01-23 10:40:40 +0100

    feat: initial auth implementation

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import config from "../config/index";
+import routes from "./routes/auth";
+import 'dotenv/config';
 
 const app = express();
+
+app.use("/auth", routes)
 
 app.listen(config.port, () => console.log(`Server running on PORT ${config.port}`))
