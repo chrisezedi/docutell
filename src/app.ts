cef3aaee88edb6d3fa2c6d10ffffbd1b3b60cb7f
chrisezedi <ekenechrisezedi@gmail.com>
2025-01-27 23:28:36 +0100

    feat: swagger setup

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,26 @@
 import express from "express";
 import config from "../config/index";
 import routes from "./routes/auth";
-import 'dotenv/config';
+import "dotenv/config";
 import { errorHandler } from "./middlewares/error";
+import { serve, setup } from "swagger-ui-express";
+import { load } from "js-yaml";
+import { readFileSync } from "fs";
+import { join } from "path";
+import swaggerJSDoc, { Options } from "swagger-jsdoc";
+
+const filePath = join(__dirname, "../docs/docutell-api.yaml");
+const swaggerDefinition: any = load(readFileSync(filePath, "utf-8"));
+const options: Options = { swaggerDefinition, apis: ["'./routes/*.ts'"] };
+const specs = swaggerJSDoc(options);
 
 const app = express();
 
+app.use("/docs", serve, setup(specs));
 app.use("/auth", routes);
 
 app.use(errorHandler);
 
-app.listen(config.port, () => console.log(`Server running on PORT ${config.port}`))
+app.listen(config.port, () =>
+  console.log(`Server running on PORT ${config.port}`)
+);
