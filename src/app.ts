ac134f55f548c158eed7efd216f31054462363c0
chrisezedi <ekenechrisezedi@gmail.com>
2025-01-28 23:26:36 +0100

    feat: first pass at openapi swagger docs

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import { readFileSync } from "fs";
 import { join } from "path";
 import swaggerJSDoc, { Options } from "swagger-jsdoc";
 
-const filePath = join(__dirname, "../docs/docutell-api.yaml");
+const filePath = join(__dirname, "../docs/openapi.yaml");
 const swaggerDefinition: any = load(readFileSync(filePath, "utf-8"));
 const options: Options = { swaggerDefinition, apis: ["'./routes/*.ts'"] };
 const specs = swaggerJSDoc(options);
